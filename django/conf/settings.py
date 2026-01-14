4370403eca2ab83d1e11b81ed5cf0752296dd32f
Jungsu Lee <jungsuu00@naver.com>
2026-01-14 16:43:37 +0900

    Clear database password in settings.py

--- django/conf/settings.py
+++ django/conf/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.mysql",
         "NAME": "perfume",
         "USER": "root",
-        "PASSWORD": "7078",
+        "PASSWORD": "",
         "HOST": "localhost",
         "PORT": "3306",
     }
